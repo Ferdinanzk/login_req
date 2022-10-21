34e113d45b17218cb6f68e59c3f60005c75642ec
Ferdinanzk <ferdinanzk@gmail.com>
2022-10-21 09:42:21 +0700

    progress multi user

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix'=>config('admin.prefix'),
+    'namespace'=>'App\\Http\\Controllers',
+],function () {
+
+    Route::get('login','LoginAdminController@formLogin')->name('admin.login');
+    Route::post('login','LoginAdminController@login');
+
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::post('logout','LoginAdminController@logout')->name('admin.logout');
+        Route::view('/','dashboard')->name('dashboard');
+        Route::view('/post','data-post')->name('post')->middleware('can:role,"admin","editor"');
+        Route::view('/admin','data-admin')->name('admin')->middleware('can:role,"admin"');
+    });
+});
